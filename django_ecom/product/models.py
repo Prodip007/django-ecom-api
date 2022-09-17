f7e093cb2e1d5ec00631e6607d4d2388f6fc2c9c
Prodip <p.k.sorkar007@gmail.com>
2022-09-17 17:24:23 +0600

    There is a problem with urls

--- django_ecom/product/models.py
+++ django_ecom/product/models.py
@@ -37,4 +37,34 @@ class Product(models.Model):
 
 
     def get_absolute_url(self):
-        return f'/{self.slug}'
+        return f'/{self.category.slug}/{self.slug}'
+
+    def get_image(self):
+        if self.image:
+            return 'http://localhost:8000/' + self.image.url
+        return ''
+
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return 'http://localhost:8000/' + self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+
+                return 'http://localhost:8000/' + self.thumbnail.url
+            else:
+                return ''
+
+    def make_thumbnail(self, image, size=(300, 200)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+
+        return thumbnail
+    
